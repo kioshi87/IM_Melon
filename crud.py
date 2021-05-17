d603acfd3a369dde4d33addc15d0a22b0d74059d
vagrant <vagrant@vagrant.vm>
2021-05-17 20:22:27 +0000

    more functions

--- crud.py
+++ crud.py
@@ -22,38 +22,36 @@ def create_messages( subscriber_number, melon_name, qty_alert, type_alert, seaso
     db.session.add(message)
     db.session.commit()
 
-def get_subscriber():
+def get_all_subscriber():
     """Return all users."""
-
     return Subscribers.query.all()
 
 def get_subscriber_by_number(subscriber_number):
-    """Return a user by primary key."""
+    """Return a user by number."""
     return Subscribers.query.get(subscriber_number)
 
 def get_subscriber_by_email(email):
     """Return a user by email."""
     return Subscribers.query.filter(Subscribers.email == email).first()
 
-def get_alerts():
-    """Return all users."""
-
+def get_all_alerts():
+    """Return all alerts"""
     return AlertSystem.query.all()
 
 def get_alerts_by_melon_name(melon_name):
-    """Return a user by email."""
+    """Return alert by melon type"""
     return Melons.query.filter(Melons.melon_name == melon_name).first()
 
-def get_messages(quantity_alert):
-    """Return a user by email."""
+def get_messages_quantity(quantity_alert):
+    """Return message by quantity"""
     return Messages.query.filter(Melons.quantity_alert== quantity_alert).first()
 
-def get_messages(type_alert):
-    """Return a user by email."""
+def get_messages_type(type_alert):
+    """Return message by type"""
     return Messages.query.filter(Melons.type_alert== type_alert).first()
 
-def get_messages(season_alert):
-    """Return a user by email."""
+def get_messages_season(season_alert):
+    """Return message by season"""
     return Messages.query.filter(Melons.season_alert== season_alert).first()
 
 
