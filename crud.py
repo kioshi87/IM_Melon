2a4d85eedba16e54d4056094eb82d1716f874556
ksusmi <susmi.k@gmail.com>
2021-05-21 19:20:24 +0000

    updated seed.py,crud.py, model.py, melons.tsv

--- crud.py
+++ crud.py
@@ -1,39 +1,23 @@
-# create a new melon record, new subsscriber, subscriber is in
-from model import db, AlertSystem,  Melons,   Messages, Subscribers,  connect_to_db 
-# from sqlalchemy.sql import func, cast
-# from datetime import datetime, date
-import sqlalchemy
+
+from model import db,  Melon, Subscriber,  connect_to_db 
+
 
 """CRUD operations."""
 def create_melons(melon_name, melon_qty,melon_type, melon_season):
     """Create and return a new melon."""
-    melon = Melons(melon_name=melon_name, melon_qty=melon_qty,melon_type=melon_type, melon_season=melon_season)
+    melon = Melon(melon_name=melon_name, melon_qty=melon_qty,melon_type=melon_type, melon_season=melon_season)
     db.session.add(melon)
     db.session.commit()
 
     return melon
 
-def create_subscribers(email, password, address, subscriber_number):
+def create_subscribers(email, password, name, subscriber_number):
     """Create and return a new user."""
-    subcriber= Subscribers(email=email, password=password, address=address, subscriber_number=subscriber_number)
+    subcriber= Subscriber(email=email, password=password, name= name,subscriber_number=subscriber_number)
     db.session.add(subcriber)
     db.session.commit()
 
     return subcriber
-
-def create_alert(id, melon_name, subscriber_number, message_id):
-    alert = AlertSystem(id=id, melon_name=melon_name, subscriber_number=subscriber_number, message_id=message_id)
-    db.session.add(alert)
-    db.session.commit()
-
-def create_messages( subscriber_number, melon_name, qty_alert, type_alert, season_alert):
-    message= AlertSystem(melon_name=melon_name, subscriber_number=subscriber_number, season_alert=season_alert, qty_alert=qty_alert, type_alert=type_alert)
-    db.session.add(message)
-    db.session.commit()
-
-def get_all_subscriber():
-    """Return all users."""
-    return Subscribers.query.all()
 
 def get_all_subscriber_numbers():
     """List of all subscriber numbers"""
@@ -44,35 +28,21 @@ def get_all_subscriber_numbers():
         number = subscriber.subscriber_number
         phone_numbers.append(number)
     
-    return phone_numbers 
+    return phone_numbers
+    
+def get_all_subscriber():
+    """Return all users."""
+    return Subscriber.query.all()
 
-def get_subscriber_by_number(subscriber_number):
-    """Return a user by number."""
-    return Subscribers.query.get(subscriber_number)
 
-def get_subscriber_by_email(email):
-    """Return a user by email."""
-    return Subscribers.query.filter(Subscribers.email == email).first()
 
-def get_all_alerts():
-    """Return all alerts"""
-    return AlertSystem.query.all()
 
-def get_alerts_by_melon_name(melon_name):
-    """Return alert by melon type"""
-    return Melons.query.filter(Melons.melon_name == melon_name).first()
 
-def get_messages_quantity(quantity_alert):
-    """Return message by quantity"""
-    return Messages.query.filter(Melons.quantity_alert== quantity_alert).first()
+# def get_subscriber_by_email(email):
+#     """Return a user by email."""
+#     return Subscribers.query.filter(Subscribers.email == email).first()
 
-def get_messages_type(type_alert):
-    """Return message by type"""
-    return Messages.query.filter(Melons.type_alert== type_alert).first()
 
-def get_messages_season(season_alert):
-    """Return message by season"""
-    return Messages.query.filter(Melons.season_alert== season_alert).first()
 
 
 if __name__ == '__main__':
--- seed.py
+++ seed.py
@@ -2,9 +2,6 @@
 
 import os
 import json
-from random import choice, randint
-from datetime import datetime
-
 import crud
 import model
 import server
@@ -16,89 +13,11 @@ os.system('createdb melon')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-# Load coupon data from CSV file, create coupon objects, load into db
 with open('melons.tsv') as tsv_f:
     f = csv.reader(tsv_f, delimiter="\t")
     
     for line in f:
-        # melon_name, melon_qty, melon_type, melon_season=line
-        # melon=crud.create_melons(melon_name,melon_qty,melon_type,melon_season)
-        print('*******************************')
+
         melon_name, melon_qty, melon_type, melon_season = line
         melon = crud.create_melons(melon_name, melon_qty, melon_type, melon_season)
     
-
-
-# import os
-# import csv
-# import crud #note, not currently a file
-# from server import app
-# from model import Melon, Subscriber, db, connect_to_db
-
-# with open('data/melons.tsv') as tsv_f:
-# 	f = csv.reader(tsv_f, delimiter="\t")
-# 	count = 1
-
-
-# def cast_int(str):
-#     try:
-#         n = int(str)
-#         return n
-#     except (ValueError, TypeError):
-#         return None
-
-
-# def seed_csv_data(filename):
-
-
-# # with open('data/melons.csv') as csv_file:
-# with open(filename, 'r') as csv_file:
-
-#     #file = csv.reader(csv_file)
-#     file = csv.DictReader(csv_file)
-
-#     for row in file:
-#         for key in row:
-#             if row[key] == '':
-#                 row[key] = None
-#     melons = Melon (
-#         # subscriber_number = cast_int(row['subscriber_number']),
-#         melon_name =  row['melon_name'],
-#         melon_qty = cast_int(row['melon_qty']),
-#         melon_type = row['melon_type'],
-#         melon_season = row['melon_season']
-#         )
-#     db.session.add(melons)
-#     db.session.commit()
-
-#     print("################")
-#     # melon = crud.create_melons(melon_name, melon_qty, melon_type, melon_season)
-#     # print("################", melon)
-
-# csv_file.close()
-
-
-# def seed_users():
-#     for i in range(9):
-#         name = fake.name().split()
-#         username = ''.join(name)
-#         password = fake.password()
-#         subscriber_number = fake.subscriber_number()
-#         email = fake.email()
-#         city = fake.city()
-#         state = fake.state()
-#         crud.create_user(username, password, email, city, state)
-
-
-# if __name__ == '__main__':
-
-
-#     # Call connect_to_db(app, echo=False) if your program output gets
-#     # too annoying; this will tell SQLAlchemy not to print out every
-#     # query it executes.
-
-#     os.system('dropdb melon')
-#     os.system('createdb melon')
-#     connect_to_db(app)
-#     db.create_all()
-# seed_csv_data('data/melons.csv')
